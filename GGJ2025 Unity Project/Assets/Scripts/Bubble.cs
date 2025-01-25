f69d91d0dc6c0e483bc69d5529f52b7b98a19116
osa <onao329876@gmail.com>
2025-01-25 12:31:45 +0900

    シャボン玉で利用してほしいメソッドを定義

--- GGJ2025 Unity Project/Assets/Scripts/Bubble.cs
+++ GGJ2025 Unity Project/Assets/Scripts/Bubble.cs
@@ -10,13 +10,47 @@ public class Bubble : MonoBehaviour
 
     }
 
-    public void Init(BubbleType type, int size)
+    /// <summary>
+    /// シャボンどおしが重なっているか
+    /// </summary>
+    public bool IsOverlap { get; private set; }
+
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    /// <param name="type">シャボン玉種別</param>
+    /// <param name="size">シャボン玉のサイズ3段階</param>
+    public void Initialize(BubbleType type, int size)
     {
 
     }
 
     // Update is called once per frame
     void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Focusされている
+    /// </summary>
+    public void OnFocus(ref bool isFocus)
+    {
+
+    }
+
+    /// <summary>
+    /// バブル破壊メソッド
+    /// </summary>
+    public void BreakBubble()
+    {
+
+    }
+
+    /// <summary>
+    /// シャボンどうしを合成
+    /// </summary>
+    public void BlendBubbles()
     {
 
     }
